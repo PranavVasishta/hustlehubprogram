a5a5471bf129c7ac6902c90a6dc61acf70cf9774
PranavVasishta <90087498+PranavVasishta@users.noreply.github.com>
2022-12-26 10:11:58 +0530

    Add files via upload

---
+++ decimal to hex.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+    int d;
+    puts("Enter decimal value");
+    scanf("%d",&d);
+    printf("hexa=%x",d);
+}
---
+++ float.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+    float a=1.0/2,b=1.0/2,c;
+    c=a+b;
+    printf("%.2f",c);
+}
